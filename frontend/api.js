e0ff8a819ac25be81ae7cf4f029af2b846401ff6
Kevin Buxton <kevinbuxton2005@gmail.com>
2025-10-30 04:03:16 +0300

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:3001'; // Changed to base URL without /api
+const BASE_URL = 'https://mercure-group.onrender.com'; // Changed to base URL without /api
 
 export const api = axios.create({
   baseURL: BASE_URL,
@@ -279,3 +279,4 @@ export const getAutomationMembers = () => {
 
 
 export default api;
+
